4d685f1e2d2ab79fdf77bdff5e6fe476556b354c
Subha5714 <154600945+Subha5714@users.noreply.github.com>
2026-04-01 21:56:50 +0530

    Update student_database.sql

--- student_database.sql
+++ student_database.sql
@@ -31,9 +31,6 @@ SELECT course, AVG(marks) FROM students GROUP BY course;
 -- Get students between 70 and 90 marks
 SELECT * FROM students WHERE marks BETWEEN 70 AND 90;
 
--- Students with marks > 80
-SELECT name, marks FROM students WHERE marks > 80;
-
 -- Average marks by course
 SELECT course, AVG(marks) FROM students GROUP BY course;
 -- Create courses table
@@ -54,6 +51,27 @@ INSERT INTO courses VALUES
 -- Join students with courses
 SELECT students.name, 
 courses.course_name
+FROM students
+INNER JOIN courses
+ON students.student_id = courses.student_id;
+
+-- Create courses table
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50),
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
+
+-- Insert course data
+INSERT INTO courses VALUES
+(101, 'DBMS', 1),
+(102, 'SQL', 2),
+(103, 'Web Dev', 3),
+(104, 'DSA', 4);
+
+-- Join students with courses
+SELECT students.name, courses.course_name
 FROM students
 INNER JOIN courses
 ON students.student_id = courses.student_id;
